2100d92525dd8cb8f8c849ee6493ca740ee69df2
Arafat <arafatnounagnonalabi@gmail.com>
2024-07-05 10:32:36 -1200

    Last Update

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,19 @@ public class Main {
     public static void main(String[] args)
     {
 
+
         System.out.println("Enter the text:");
         // next 2 lines should be merged in one
-        Scanner s = new Scanner(System.in);
-        String text = s.nextLine();
+
+        String text = new Scanner(System.in).nextLine();
 
         // use string.split function to get the result u need
 
-        System.out.println("Number of word(s): "+counter);
+        String [] ArrayOfText = text.split(" ");
+
+
+            System.out.println("Number of Words: "+ArrayOfText.length);
+
 
 
     }
